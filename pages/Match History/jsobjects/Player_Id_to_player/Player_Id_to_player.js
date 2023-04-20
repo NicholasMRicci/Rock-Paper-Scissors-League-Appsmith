c440d9578ed0eb4b730a9873313f1fb2fd10cccf
Sean Seibel <seibel.s@northeastern.edu>
2023-04-20 00:03:00 +0000

    names for match history

---
+++ pages/Match History/jsobjects/Player_Id_to_player/Player_Id_to_player.js
@@ -0,0 +1,27 @@
+export default {
+	getMatches() {
+		let players = Get_Players.data
+		let plMap = new Map(players.map((pl) => {
+			return [pl.playerID, pl.firstName + " " + pl.lastName]
+		}))
+		let tournaments = Tournaments.data
+		let tMap = new Map(tournaments.map((t) => {
+			return [t.tournamentID, t.tournamentName]
+		}))
+		let matches = Matches.data
+		return matches.map((match) => {
+			return {
+				"player1": plMap.get(match.player1ID),
+				"player1Throw": match.player1Throw,
+				"player2": plMap.get(match.player2ID),
+				"player2Throw": match.player2Throw,
+				"team1": match.team1,
+				"team2": match.team2,
+				"tournament": tMap.get(match.tournamentID),
+				"gameID": match.gameID
+			}
+		})
+		
+	}
+	
+}
